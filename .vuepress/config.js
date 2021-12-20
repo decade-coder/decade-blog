420a0a02f1721375bb66f7eea8763b661f6e2bd3
decade-coder <1596682774@qq.com>
2021-12-20 23:47:02 +0800

    初始化配置

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,79 +1,100 @@
 module.exports = {
-  title: "vuepress-theme-reco",
-  description: 'A simple and beautiful vuepress blog theme .',
-  dest: 'public',
+  title: "Decade的博客",
+  description: "A simple and beautiful vuepress blog theme .",
+  dest: "public",
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
-  theme: 'reco',
+  theme: "reco",
+  // 多语言配置
+  // locales: {
+  //   "/": {
+  //     lang: "zh-CN",
+  //   },
+  //   "/en/": {
+  //     lang: "en-US",
+  //   },
+  // },
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/', icon: 'reco-home' },
-      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-      { text: 'Docs', 
-        icon: 'reco-message',
-        items: [
-          { text: 'vuepress-reco', link: '/docs/theme-reco/' }
-        ]
+      { text: "Home", link: "/", icon: "reco-home" },
+      // 时间轴
+      { text: "TimeLine", link: "/timeline/", icon: "reco-date" },
+      {
+        text: "Docs",
+        icon: "reco-message",
+        items: [{ text: "vuepress-reco", link: "/docs/theme-reco/" }],
       },
-      { text: 'Contact', 
-        icon: 'reco-message',
+      {
+        text: "Contact",
+        icon: "reco-message",
         items: [
-          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' }
-        ]
-      }
+          {
+            text: "GitHub",
+            link: "https://github.com/recoluan",
+            icon: "reco-github",
+          },
+        ],
+      },
     ],
     sidebar: {
-      '/docs/theme-reco/': [
-        '',
-        'theme',
-        'plugin',
-        'api'
-      ]
-    },  
-    type: 'blog',
+      "/docs/theme-reco/": ["", "theme", "plugin", "api"],
+    },
+    // 此处可以设置为自定义vue组件
+    type: "blog",
     // 博客设置
     blogConfig: {
       category: {
         location: 2, // 在导航栏菜单中所占的位置，默认2
-        text: 'Category' // 默认 “分类”
+        text: "Category", // 默认 “分类”
       },
       tag: {
         location: 3, // 在导航栏菜单中所占的位置，默认3
-        text: 'Tag' // 默认 “标签”
-      }
+        text: "Tag", // 默认 “标签”
+      },
     },
-    friendLink: [
-      {
-        title: '午后南杂',
-        desc: 'Enjoy when you can, and endure when you must.',
-        email: '1156743527@qq.com',
-        link: 'https://www.recoluan.com'
-      },
-      {
-        title: 'vuepress-theme-reco',
-        desc: 'A simple and beautiful vuepress Blog & Doc theme.',
-        avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        link: 'https://vuepress-theme-reco.recoluan.com'
-      },
-    ],
-    logo: '/logo.png',
+    // 添加友链
+    // friendLink: [
+    //   {
+    //     title: "vuepress-theme-reco",
+    //     desc: "A simple and beautiful vuepress Blog & Doc theme.",
+    //     avatar:
+    //       "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+    //     link: "https://vuepress-theme-reco.recoluan.com",
+    //   },
+    // ],
+    // 导航栏左侧logo
+    logo: "/logo.png",
+    // 评论设置
+    vssueConfig: {
+      platform: "github",
+      owner: "OWNER_OF_REPO",
+      repo: "NAME_OF_REPO",
+      clientId: "YOUR_CLIENT_ID",
+      clientSecret: "YOUR_CLIENT_SECRET",
+    },
     // 搜索设置
     search: true,
     searchMaxSuggestions: 10,
     // 自动形成侧边导航
     // sidebar: 'auto',
     // 最后更新时间
-    lastUpdated: 'Last Updated',
+    lastUpdated: "Last Updated",
     // 作者
-    author: 'reco_luan',
+    author: "Decade",
     // 作者头像
-    authorAvatar: '/avatar.png',
+    authorAvatar: "/avatar.png",
     // 备案号
-    record: 'xxxx',
+    record: "ICP 备案文案",
     // 项目开始时间
-    startYear: '2017'
+    startYear: "2021",
     /**
      * 密钥 (if your blog is private)
      */
@@ -94,6 +115,6 @@ module.exports = {
     // }
   },
   markdown: {
-    lineNumbers: true
-  }
-}  
+    lineNumbers: true,
+  },
+};
